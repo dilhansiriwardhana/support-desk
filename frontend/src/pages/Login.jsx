0e271900c58926bacec45b7acae3c6d744e31707
dilhansiriwardhana <76891526+dilhansiriwardhana@users.noreply.github.com>
2023-09-18 21:06:00 +0530

    login functionality added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,11 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+
+import { useNavigate } from 'react-router-dom'
 import {FaSignInAlt} from 'react-icons/fa'
 import {toast} from 'react-toastify'
+import {useSelector, useDispatch } from 'react-redux'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -12,6 +17,25 @@ function Login() {
 
     const { email, password} = formData
 
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+         toast.error(message)
+        } 
+ 
+        //   Redirect when logged in
+        if(isSuccess || user) {
+             navigate('/')
+        }
+ 
+        dispatch(reset())
+ 
+     }, [isError, isSuccess, user, message, navigate, dispatch])
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -22,9 +46,17 @@ function Login() {
     const onSubmit = (e) => {
         e.preventDefault()
 
+        const userData = {
+            email,
+            password,
+        }
 
+        dispatch(login(userData))
 
-       
+    }
+
+    if(isLoading) {
+        return <Spinner />
     }
 
     return (
