8660ee9cc5d0a7b21e1213aefc0eece5b39d8eb8
dilhansiriwardhana <76891526+dilhansiriwardhana@users.noreply.github.com>
2023-09-20 15:25:59 +0530

    heroku auto run code defined

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 //  Serve frontend
 if(process.env.NODE_ENV === 'production') {
     // Set build folder as static
-    app.use(express.static(path.join(_dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*' (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html') )
 } else {
